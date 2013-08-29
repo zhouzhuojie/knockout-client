550e9293ab933573db8661c780839ba056f3d883
zhouzhuojie <zhouzhuojie@gmail.com>
2013-08-29 00:43:07 -0400

    Safe export with main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+if (typeof Package !== "undefined") ko = this.ko;
--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.add_files('lib/knockout/knockout.js', 'client');
+  api.add_files('main.js', 'client');
   api.add_files('lib/knockout/knockout.mapping.js', 'client');
   api.add_files('lib/knockout.validation/knockout.validation.js', 'client');
   api.add_files('lib/knockout.validation/en-US.js', 'client');
